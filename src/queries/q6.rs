86611ca50765beca67478710c305f483bb145fab
Matthew Brookes <brookesm@student.ethz.ch>
2019-07-03 14:10:38 +0200

    Q6: use ManagedState

--- src/queries/q6.rs
+++ src/queries/q6.rs
@@ -1,9 +1,19 @@
+use faster_rs::FasterRmw;
 use std::collections::VecDeque;
 use timely::dataflow::channels::pact::Exchange;
 use timely::dataflow::operators::{Map, Operator};
 use timely::dataflow::{Scope, Stream};
 
-use {crate::queries::NexmarkInput, crate::queries::NexmarkTimer};
+use crate::queries::{NexmarkInput, NexmarkTimer};
+
+#[derive(Serialize, Deserialize)]
+struct Prices(VecDeque<usize>);
+
+impl FasterRmw for Prices {
+    fn rmw(&self, _modification: Self) -> Self {
+        panic!("RMW on VecDeque<T> is unsafe");
+    }
+}
 
 pub fn q6<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
@@ -16,21 +26,23 @@ pub fn q6<S: Scope<Timestamp = usize>>(
         .unary(
             Exchange::new(|x: &(usize, usize)| x.0 as u64),
             "Q6 Average",
-            |_cap, _info, _state_handle| {
+            |_cap, _info, state_handle| {
                 // Store bidder -> [prices; 10]
-                let mut state = std::collections::HashMap::new();
+                let mut state = state_handle.get_managed_map("state");
 
                 move |input, output| {
                     input.for_each(|time, data| {
                         let mut session = output.session(&time);
                         for (bidder, price) in data.iter().cloned() {
-                            let entry = state.entry(bidder).or_insert_with(VecDeque::new);
+                            let mut entry =
+                                state.remove(&bidder).unwrap_or(Prices(VecDeque::new())).0;
                             if entry.len() >= 10 {
                                 entry.pop_back();
                             }
                             entry.push_front(price);
                             let sum: usize = entry.iter().sum();
                             session.give((bidder, sum / entry.len()));
+                            state.insert(bidder, Prices(entry));
                         }
                     });
                 }
