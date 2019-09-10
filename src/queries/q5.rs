47426a5ed149ab3bfa809c799456650cf55c1276
Matthew Brookes <mbrookes1304@gmail.com>
2019-09-10 10:34:18 +0100

    Q5: additions and deletions hand-tuned

--- src/queries/q5.rs
+++ src/queries/q5.rs
@@ -5,7 +5,10 @@ use timely::dataflow::{Scope, Stream};
 
 use crate::event::Date;
 
+use super::maybe_refresh_faster;
 use crate::queries::{NexmarkInput, NexmarkTimer};
+use tempfile::TempDir;
+use faster_rs::{FasterKv, status};
 
 pub fn q5<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
@@ -14,8 +17,22 @@ pub fn q5<S: Scope<Timestamp = usize>>(
     window_slice_count: usize,
     window_slide_ns: usize,
 ) -> Stream<S, usize> {
-    let mut additions = HashMap::new();
-    let mut deletions = HashMap::new();
+    //let mut additions = HashMap::new();
+    let additions_directory = TempDir::new_in(".").unwrap().into_path();
+    let additions = FasterKv::new_auctions_store(
+        1 << 15,
+        2 * 1024 * 1024 * 1024,
+        additions_directory.to_str().unwrap().to_string()
+    ).unwrap();
+    let mut additions_store_serial = 0;
+    //let mut deletions = HashMap::new();
+    let deletions_directory = TempDir::new_in(".").unwrap().into_path();
+    let deletions = FasterKv::new_auctions_store(
+        1 << 15,
+        2 * 1024 * 1024 * 1024,
+        deletions_directory.to_str().unwrap().to_string()
+    ).unwrap();
+    let mut deletions_store_serial = 0;
     let mut accumulations = HashMap::new();
 
     input
@@ -44,35 +61,58 @@ pub fn q5<S: Scope<Timestamp = usize>>(
                     // Collect all bids in a different slide.
                     for &(auction, a_time) in bids_buffer.iter() {
                         if a_time != slide {
+                            additions.rmw_auction(nt.from_nexmark_time(a_time) as u64, auction as u64, additions_store_serial);
+                            maybe_refresh_faster(&additions, &mut additions_store_serial);
+                            /*
                             additions
                                 .entry(time.delayed(&nt.from_nexmark_time(a_time)))
                                 .or_insert_with(Vec::new)
                                 .push(auction);
+                            */
                             notificator.notify_at(time.delayed(&nt.from_nexmark_time(a_time)));
                         }
                     }
                     bids_buffer.retain(|&(_, a_time)| a_time == slide);
 
                     // Collect all bids in the same slide.
+                    let bids = bids_buffer.drain(..).map(|(b, _)| b as u64).collect();
+                    additions.rmw_auctions(nt.from_nexmark_time(slide) as u64, bids, additions_store_serial);
+                    maybe_refresh_faster(&additions, &mut additions_store_serial);
+                    /*
                     additions
                         .entry(downgrade)
                         .or_insert_with(Vec::new)
                         .extend(bids_buffer.drain(..).map(|(b, _)| b));
+                    */
                 });
 
                 notificator.for_each(|time, _, notificator| {
-                    if let Some(additions) = additions.remove(&time) {
-                        for &auction in additions.iter() {
-                            *accumulations.entry(auction).or_insert(0) += 1;
+                    let (status, recv) = additions.read_auctions(*time.time() as u64, additions_store_serial);
+                    if status == status::PENDING {
+                        additions.complete_pending(true);
+                    }
+                    //if let Some(additions) = additions.remove(&time) {
+                    if let Ok(additions) = recv.recv() {
+                        for auction in additions.iter() {
+                            *accumulations.entry(*auction).or_insert(0) += 1;
                         }
                         let new_time = time.time() + (window_slice_count * window_slide_ns);
-                        deletions.insert(time.delayed(&new_time), additions);
+                        //deletions.insert(time.delayed(&new_time), additions);
+                        let mut copied_additions = Vec::with_capacity(additions.len());
+                        copied_additions.extend_from_slice(additions);
+                        deletions.upsert_auctions(new_time as u64, copied_additions, deletions_store_serial);
+                        maybe_refresh_faster(&deletions, &mut deletions_store_serial);
                         notificator.notify_at(time.delayed(&new_time));
                     }
-                    if let Some(deletions) = deletions.remove(&time) {
-                        for auction in deletions.into_iter() {
+                    let (status, recv) = deletions.read_auctions(*time.time() as u64, deletions_store_serial);
+                    if status == status::PENDING {
+                        deletions.complete_pending(true);
+                    }
+                    //if let Some(deletions) = deletions.remove(&time) {
+                    if let Ok(deletions) = recv.recv() {
+                        for auction in deletions {
                             use std::collections::hash_map::Entry;
-                            match accumulations.entry(auction) {
+                            match accumulations.entry(*auction) {
                                 Entry::Occupied(mut entry) => {
                                     *entry.get_mut() -= 1;
                                     if *entry.get_mut() == 0 {
@@ -86,7 +126,7 @@ pub fn q5<S: Scope<Timestamp = usize>>(
                     if let Some((count, auction)) =
                         accumulations.iter().map(|(&a, &c)| (c, a)).max()
                     {
-                        output.session(&time).give((auction, count));
+                        output.session(&time).give((auction as usize, count));
                     }
                 })
             },
