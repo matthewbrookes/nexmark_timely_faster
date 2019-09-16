7c44f8695472884b7171995e359c155993fdddfb
Matthew Brookes <mbrookes1304@gmail.com>
2019-09-16 15:41:56 +0100

    Q5: hand tuned

--- src/queries/q5.rs
+++ src/queries/q5.rs
@@ -7,8 +7,8 @@ use crate::event::Date;
 
 use super::maybe_refresh_faster;
 use crate::queries::{NexmarkInput, NexmarkTimer};
+use faster_rs::{status, FasterKv};
 use tempfile::TempDir;
-use faster_rs::{FasterKv, status};
 
 pub fn q5<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
@@ -22,24 +22,35 @@ pub fn q5<S: Scope<Timestamp = usize>>(
     let additions = FasterKv::new_auctions_store(
         1 << 15,
         2 * 1024 * 1024 * 1024,
-        additions_directory.to_str().unwrap().to_string()
-    ).unwrap();
+        additions_directory.to_str().unwrap().to_string(),
+    )
+    .unwrap();
     let mut additions_store_serial = 0;
     //let mut deletions = HashMap::new();
     let deletions_directory = TempDir::new_in(".").unwrap().into_path();
     let deletions = FasterKv::new_auctions_store(
         1 << 15,
         2 * 1024 * 1024 * 1024,
-        deletions_directory.to_str().unwrap().to_string()
-    ).unwrap();
+        deletions_directory.to_str().unwrap().to_string(),
+    )
+    .unwrap();
     let mut deletions_store_serial = 0;
     let accumulations_directory = TempDir::new_in(".").unwrap().into_path();
     let accumulations = FasterKv::new_u64_store(
         1 << 15,
         2 * 1024 * 1024 * 1024,
-        accumulations_directory.to_str().unwrap().to_string()
-    ).unwrap();
+        accumulations_directory.to_str().unwrap().to_string(),
+    )
+    .unwrap();
     let mut accumulations_store_serial = 0;
+    let hot_items_directory = TempDir::new_in(".").unwrap().into_path();
+    let hot_items = FasterKv::new_u64_pair_store(
+        1 << 15,
+        2 * 1024 * 1024 * 1024,
+        hot_items_directory.to_str().unwrap().to_string(),
+    )
+    .unwrap();
+    let mut hot_items_store_serial = 0;
 
     input
         .bids(scope)
@@ -67,14 +78,12 @@ pub fn q5<S: Scope<Timestamp = usize>>(
                     // Collect all bids in a different slide.
                     for &(auction, a_time) in bids_buffer.iter() {
                         if a_time != slide {
-                            additions.rmw_auction(nt.from_nexmark_time(a_time) as u64, auction as u64, additions_store_serial);
+                            additions.rmw_auction(
+                                nt.from_nexmark_time(a_time) as u64,
+                                auction as u64,
+                                additions_store_serial,
+                            );
                             maybe_refresh_faster(&additions, &mut additions_store_serial);
-                            /*
-                            additions
-                                .entry(time.delayed(&nt.from_nexmark_time(a_time)))
-                                .or_insert_with(Vec::new)
-                                .push(auction);
-                            */
                             notificator.notify_at(time.delayed(&nt.from_nexmark_time(a_time)));
                         }
                     }
@@ -82,46 +91,47 @@ pub fn q5<S: Scope<Timestamp = usize>>(
 
                     // Collect all bids in the same slide.
                     let bids = bids_buffer.drain(..).map(|(b, _)| b as u64).collect();
-                    additions.rmw_auctions(nt.from_nexmark_time(slide) as u64, bids, additions_store_serial);
+                    additions.rmw_auctions(
+                        nt.from_nexmark_time(slide) as u64,
+                        bids,
+                        additions_store_serial,
+                    );
                     maybe_refresh_faster(&additions, &mut additions_store_serial);
-                    /*
-                    additions
-                        .entry(downgrade)
-                        .or_insert_with(Vec::new)
-                        .extend(bids_buffer.drain(..).map(|(b, _)| b));
-                    */
                 });
 
                 notificator.for_each(|time, _, notificator| {
-                    let (status, recv) = additions.read_auctions(*time.time() as u64, additions_store_serial);
+                    let (status, recv) =
+                        additions.read_auctions(*time.time() as u64, additions_store_serial);
                     if status == status::PENDING {
                         additions.complete_pending(true);
                     }
                     maybe_refresh_faster(&additions, &mut additions_store_serial);
-                    //if let Some(additions) = additions.remove(&time) {
                     if let Ok(additions) = recv.recv() {
                         for auction in additions.iter() {
-                            //*accumulations.entry(*auction).or_insert(0) += 1;
                             accumulations.rmw_u64(*auction, 1, accumulations_store_serial);
                             maybe_refresh_faster(&accumulations, &mut accumulations_store_serial);
                         }
                         let new_time = time.time() + (window_slice_count * window_slide_ns);
-                        //deletions.insert(time.delayed(&new_time), additions);
                         let mut copied_additions = Vec::with_capacity(additions.len());
                         copied_additions.extend_from_slice(additions);
-                        deletions.upsert_auctions(new_time as u64, copied_additions, deletions_store_serial);
+                        deletions.upsert_auctions(
+                            new_time as u64,
+                            copied_additions,
+                            deletions_store_serial,
+                        );
                         maybe_refresh_faster(&deletions, &mut deletions_store_serial);
                         notificator.notify_at(time.delayed(&new_time));
                     }
-                    let (status, recv) = deletions.read_auctions(*time.time() as u64, deletions_store_serial);
+                    let (status, recv) =
+                        deletions.read_auctions(*time.time() as u64, deletions_store_serial);
                     if status == status::PENDING {
                         deletions.complete_pending(true);
                     }
                     maybe_refresh_faster(&deletions, &mut deletions_store_serial);
-                    //if let Some(deletions) = deletions.remove(&time) {
                     if let Ok(deletions) = recv.recv() {
                         for auction in deletions {
-                            let (status, recv) = accumulations.read_u64(*auction, accumulations_store_serial);
+                            let (status, recv) =
+                                accumulations.read_u64(*auction, accumulations_store_serial);
                             if status == status::PENDING {
                                 accumulations.complete_pending(true);
                             }
@@ -129,26 +139,22 @@ pub fn q5<S: Scope<Timestamp = usize>>(
                             match recv.recv() {
                                 Ok(entry) => {
                                     if entry == 1 {
-                                        accumulations.delete_u64(*auction, accumulations_store_serial);
+                                        accumulations
+                                            .delete_u64(*auction, accumulations_store_serial);
                                     } else {
-                                        accumulations.rmw_decrease_u64(*auction, 1, accumulations_store_serial);
+                                        accumulations.rmw_decrease_u64(
+                                            *auction,
+                                            1,
+                                            accumulations_store_serial,
+                                        );
                                     }
-                                    maybe_refresh_faster(&accumulations, &mut accumulations_store_serial);
-                                },
+                                    maybe_refresh_faster(
+                                        &accumulations,
+                                        &mut accumulations_store_serial,
+                                    );
+                                }
                                 Err(_) => panic!("entry has to exist"),
                             }
-                            /*
-                            use std::collections::hash_map::Entry;
-                            match accumulations.entry(*auction) {
-                                Entry::Occupied(mut entry) => {
-                                    *entry.get_mut() -= 1;
-                                    if *entry.get_mut() == 0 {
-                                        entry.remove();
-                                    }
-                                }
-                                _ => panic!("entry has to exist"),
-                            }
-                            */
                         }
                     }
                     let mut highest_count: Option<(u64, u64)> = None;
@@ -163,42 +169,50 @@ pub fn q5<S: Scope<Timestamp = usize>>(
                     if let Some((count, auction)) = highest_count {
                         output.session(&time).give((auction as usize, count));
                     }
-                    /*
-                    if let Some((count, auction)) =
-                        accumulations.iter().map(|(&a, &c)| (c, a)).max()
-                    {
-                        output.session(&time).give((auction as usize, count));
-                    }
-                    */
                 })
             },
         )
-        .unary_frontier(
+        .unary_notify(
             Exchange::new(|_| 0),
             "Q5 All-Accumulate",
-            |_cap, _info| {
-                let mut hot_items = HashMap::new();
-
+            None,
+            move |input, output, notificator| {
                 let mut buffer = Vec::new();
-                move |input, output| {
-                    input.for_each(|time, data| {
-                        data.swap(&mut buffer);
-                        let current_hottest = hot_items.entry(time.retain()).or_insert((0, 0));
-                        for &(auction, count) in buffer.iter() {
-                            if count > current_hottest.1 {
-                                *current_hottest = (auction, count);
-                            }
-                        }
-                    });
-
-                    for (time, (auction, _count)) in hot_items.iter() {
-                        if !input.frontier.less_than(time.time()) {
-                            output.session(&time).give(*auction);
+                input.for_each(|time, data| {
+                    data.swap(&mut buffer);
+                    let (status, recv) =
+                        hot_items.read_u64_pair(*time.time() as u64, hot_items_store_serial);
+                    if status == status::PENDING {
+                        hot_items.complete_pending(true);
+                    }
+                    maybe_refresh_faster(&hot_items, &mut hot_items_store_serial);
+                    let mut current_hottest = match recv.recv() {
+                        Ok((left, right)) => (*left, *right),
+                        Err(_) => (0, 0),
+                    };
+                    for &(auction, count) in buffer.iter() {
+                        if count > current_hottest.1 {
+                            current_hottest = (auction as u64, count);
                         }
                     }
+                    hot_items.upsert_u64_pair(
+                        *time.time() as u64,
+                        current_hottest,
+                        hot_items_store_serial,
+                    );
+                    maybe_refresh_faster(&hot_items, &mut hot_items_store_serial);
+                    notificator.notify_at(time.delayed(&time.time()))
+                });
 
-                    hot_items.retain(move |time, _| input.frontier.less_than(time.time()));
-                }
+                notificator.for_each(|cap, _, _| {
+                    let (status, recv) = hot_items.read_u64_pair(*cap.time() as u64, hot_items_store_serial);
+                    if status == status::PENDING {
+                        hot_items.complete_pending(true);
+                    }
+                    maybe_refresh_faster(&hot_items, &mut hot_items_store_serial);
+                    output.session(&cap).give(*recv.recv().unwrap().0 as usize);
+                });
+
             },
         )
 }
