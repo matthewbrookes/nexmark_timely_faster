a39e77628c7d253dc20bf8de2e6ebf8a435195ff
Matthew Brookes <mbrookes1304@gmail.com>
2019-09-17 12:41:42 +0100

    Q6: haand-tuned

--- src/queries/q6.rs
+++ src/queries/q6.rs
@@ -1,15 +1,26 @@
-use std::collections::VecDeque;
 use timely::dataflow::channels::pact::Exchange;
 use timely::dataflow::operators::{Map, Operator};
 use timely::dataflow::{Scope, Stream};
 
+use super::maybe_refresh_faster;
 use crate::queries::{NexmarkInput, NexmarkTimer};
+use faster_rs::{status, FasterKv};
+use tempfile::TempDir;
 
 pub fn q6<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
     _nt: NexmarkTimer,
     scope: &mut S,
 ) -> Stream<S, (usize, usize)> {
+    let aggs_directory = TempDir::new_in(".").unwrap().into_path();
+    // Store bidder -> [prices; 10]
+    let aggs = FasterKv::new_u64_pair_store(
+        1 << 15,
+        2 * 1024 * 1024 * 1024,
+        aggs_directory.to_str().unwrap().to_string(),
+    )
+    .unwrap();
+    let mut aggs_store_serial = 0;
     input
         .closed_auctions(scope)
         .map(|(_a, b)| (b.bidder, b.price))
@@ -17,20 +28,19 @@ pub fn q6<S: Scope<Timestamp = usize>>(
             Exchange::new(|x: &(usize, usize)| x.0 as u64),
             "Q6 Average",
             |_cap, _info| {
-                // Store bidder -> [prices; 10]
-                let mut state = std::collections::HashMap::new();
-
                 move |input, output| {
                     input.for_each(|time, data| {
                         let mut session = output.session(&time);
                         for (bidder, price) in data.iter().cloned() {
-                            let entry = state.entry(bidder).or_insert_with(VecDeque::new);
-                            if entry.len() >= 10 {
-                                entry.pop_back();
+                            aggs.rmw_ten_elements(bidder as u64, price, aggs_store_serial);
+                            maybe_refresh_faster(&aggs, &mut aggs_store_serial);
+                            let (res, recv) =
+                                aggs.read_ten_elements_average(bidder as u64, aggs_store_serial);
+                            if res == status::PENDING {
+                                aggs.complete_pending(true);
                             }
-                            entry.push_front(price);
-                            let sum: usize = entry.iter().sum();
-                            session.give((bidder, sum / entry.len()));
+                            maybe_refresh_faster(&aggs, &mut aggs_store_serial);
+                            session.give((bidder, recv.recv().unwrap()));
                         }
                     });
                 }
