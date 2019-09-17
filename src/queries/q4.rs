754d570109178b7f3dfc03b7de6dc19c174e0da7
Matthew Brookes <mbrookes1304@gmail.com>
2019-09-17 11:46:09 +0100

    Q4: hand-tuned

--- src/queries/q4.rs
+++ src/queries/q4.rs
@@ -2,13 +2,25 @@ use timely::dataflow::channels::pact::Exchange;
 use timely::dataflow::operators::{Map, Operator};
 use timely::dataflow::{Scope, Stream};
 
+use super::maybe_refresh_faster;
 use crate::queries::{NexmarkInput, NexmarkTimer};
+use faster_rs::{status, FasterKv};
+use tempfile::TempDir;
 
 pub fn q4<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
     _nt: NexmarkTimer,
     scope: &mut S,
 ) -> Stream<S, (usize, usize)> {
+    let aggs_directory = TempDir::new_in(".").unwrap().into_path();
+    // Stores category -> (total, count)
+    let aggs = FasterKv::new_u64_pair_store(
+        1 << 15,
+        2 * 1024 * 1024 * 1024,
+        aggs_directory.to_str().unwrap().to_string(),
+    )
+    .unwrap();
+    let mut aggs_store_serial = 0;
     input
         .closed_auctions(scope)
         .map(|(a, b)| (a.category, b.price))
@@ -16,17 +28,33 @@ pub fn q4<S: Scope<Timestamp = usize>>(
             Exchange::new(|x: &(usize, usize)| x.0 as u64),
             "Q4 Average",
             |_cap, _info| {
-                // Stores category -> (total, count)
-                let mut state = std::collections::HashMap::new();
-
                 move |input, output| {
                     input.for_each(|time, data| {
                         let mut session = output.session(&time);
                         for (category, price) in data.iter().cloned() {
-                            let entry = state.entry(category).or_insert((0, 0));
-                            entry.0 += price;
-                            entry.1 += 1;
-                            session.give((category, entry.0 / entry.1));
+                            let (res, recv) =
+                                aggs.read_u64_pair(category as u64, aggs_store_serial);
+                            if res == status::PENDING {
+                                aggs.complete_pending(true);
+                            }
+                            maybe_refresh_faster(&aggs, &mut aggs_store_serial);
+                            let avg = match recv.recv() {
+                                Ok((sum, count)) => {
+                                    *sum += (price as u64);
+                                    *count += 1;
+                                    *sum / *count
+                                }
+                                Err(_) => {
+                                    aggs.upsert_u64_pair(
+                                        category as u64,
+                                        (price as u64, 1),
+                                        aggs_store_serial,
+                                    );
+                                    maybe_refresh_faster(&aggs, &mut aggs_store_serial);
+                                    price as u64
+                                }
+                            };
+                            session.give((category, avg as usize));
                         }
                     })
                 }
