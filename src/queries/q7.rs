e8f983044e1f60870bf426e7e08817244c5dd744
Matthew Brookes <mbrookes1304@gmail.com>
2019-10-18 17:16:39 +0100

    Q7: hand-tuned

--- src/queries/q7.rs
+++ src/queries/q7.rs
@@ -4,7 +4,11 @@ use timely::dataflow::{Scope, Stream};
 
 use crate::event::Date;
 
+use super::maybe_refresh_faster;
 use crate::queries::{NexmarkInput, NexmarkTimer};
+use tempfile::TempDir;
+use faster_rs::{FasterKv,status};
+use std::collections::HashSet;
 
 pub fn q7<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
@@ -22,33 +26,53 @@ pub fn q7<S: Scope<Timestamp = usize>>(
         })
         .unary_frontier(Pipeline, "Q7 Pre-reduce", |_cap, _info| {
             // Tracks the worker-local maximal bid for each capability.
-            let mut maxima = Vec::<(Capability<usize>, usize)>::new();
+            let state_directory = TempDir::new_in(".").unwrap().into_path();
+            let state = FasterKv::new_u64_store(
+                1 << 24,
+                2 * 1024 * 1024 * 1024,
+                state_directory.to_str().unwrap().to_string(),
+            )
+            .unwrap();
+            let mut state_store_serial = 0;
 
+            let mut capabilities = Vec::<Capability<usize>>::new();
             move |input, output| {
                 input.for_each(|time, data| {
                     for (window, price) in data.iter().cloned() {
-                        if let Some(position) = maxima
-                            .iter()
-                            .position(|x| *(x.0).time() == nt.from_nexmark_time(window))
-                        {
-                            if maxima[position].1 < price {
-                                maxima[position].1 = price;
-                            }
-                        } else {
-                            maxima.push((time.delayed(&nt.from_nexmark_time(window)), price));
+                        let (status, recv) = state.read_u64(nt.from_nexmark_time(window) as u64, state_store_serial);
+                        if status == status::PENDING {
+                            state.complete_pending(true);
+                        }
+                        maybe_refresh_faster(&state, &mut state_store_serial);
+                        match recv.recv() {
+                            Ok(current_highest) => {
+                                if (current_highest as usize) < price {
+                                    state.upsert_u64(nt.from_nexmark_time(window) as u64, price as u64, state_store_serial);
+                                    maybe_refresh_faster(&state, &mut state_store_serial);
+                                }
+                            },
+                            Err(_) => {
+                                state.upsert_u64(nt.from_nexmark_time(window) as u64, price as u64, state_store_serial);
+                                maybe_refresh_faster(&state, &mut state_store_serial);
+                                capabilities.push(time.delayed(&nt.from_nexmark_time(window)));
+                            },
                         }
                     }
                 });
 
-                for &(ref capability, price) in maxima.iter() {
-                    if !input.frontier.less_than(capability.time()) {
+                for &(ref cap) in capabilities.iter() {
+                    if !input.frontier.less_than(cap.time()) {
+                        let (status, recv) = state.read_u64(*cap.time() as u64, state_store_serial);
+                        if status == status::PENDING {
+                            state.complete_pending(true);
+                        }
+                        maybe_refresh_faster(&state, &mut state_store_serial);
                         output
-                            .session(&capability)
-                            .give((*capability.time(), price));
+                            .session(&cap)
+                            .give((*cap.time(), recv.recv().expect("Value must be present") as usize));
                     }
                 }
-
-                maxima.retain(|(capability, _)| input.frontier.less_than(capability));
+                capabilities.retain(|capability| input.frontier.less_than(capability));
             }
         })
         .unary_frontier(
@@ -56,30 +80,53 @@ pub fn q7<S: Scope<Timestamp = usize>>(
             "Q7 All-reduce",
             |_cap, _info| {
                 // Tracks the global maximal bid for each capability.
-                let mut maxima = Vec::<(Capability<usize>, usize)>::new();
+                let state_directory = TempDir::new_in(".").unwrap().into_path();
+                let state = FasterKv::new_u64_store(
+                    1 << 24,
+                    2 * 1024 * 1024 * 1024,
+                    state_directory.to_str().unwrap().to_string(),
+                )
+                    .unwrap();
+                let mut state_store_serial = 0;
 
+                let mut capabilities = Vec::<Capability<usize>>::new();
                 move |input, output| {
                     input.for_each(|time, data| {
                         for (window, price) in data.iter().cloned() {
-                            if let Some(position) =
-                                maxima.iter().position(|x| *(x.0).time() == window)
-                            {
-                                if maxima[position].1 < price {
-                                    maxima[position].1 = price;
-                                }
-                            } else {
-                                maxima.push((time.delayed(&window), price));
+                            let (status, recv) = state.read_u64(window as u64, state_store_serial);
+                            if status == status::PENDING {
+                                state.complete_pending(true);
+                            }
+                            maybe_refresh_faster(&state, &mut state_store_serial);
+                            match recv.recv() {
+                                Ok(current_highest) => {
+                                    if (current_highest as usize) < price {
+                                        state.upsert_u64(window as u64, price as u64, state_store_serial);
+                                        maybe_refresh_faster(&state, &mut state_store_serial);
+                                    }
+                                },
+                                Err(_) => {
+                                    state.upsert_u64(window as u64, price as u64, state_store_serial);
+                                    maybe_refresh_faster(&state, &mut state_store_serial);
+                                    capabilities.push(time.delayed(&window));
+                                },
                             }
                         }
                     });
 
-                    for &(ref capability, price) in maxima.iter() {
-                        if !input.frontier.less_than(capability.time()) {
-                            output.session(&capability).give(price);
+                    for &(ref cap) in capabilities.iter() {
+                        if !input.frontier.less_than(cap.time()) {
+                            let (status, recv) = state.read_u64(*cap.time() as u64, state_store_serial);
+                            if status == status::PENDING {
+                                state.complete_pending(true);
+                            }
+                            maybe_refresh_faster(&state, &mut state_store_serial);
+                            output
+                                .session(&cap)
+                                .give(recv.recv().expect("Value must be present") as usize);
                         }
                     }
-
-                    maxima.retain(|(capability, _)| input.frontier.less_than(capability));
+                    capabilities.retain(|capability| input.frontier.less_than(capability));
                 }
             },
         )
