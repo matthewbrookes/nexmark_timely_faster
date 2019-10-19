e19eba7f1c8c35790b45904acf03342d4a9894eb
Matthew Brookes <mbrookes1304@gmail.com>
2019-10-19 23:31:31 +0100

    Q8 hand-tuned

--- src/queries/q8.rs
+++ src/queries/q8.rs
@@ -4,9 +4,9 @@ use timely::dataflow::{Scope, Stream};
 
 use crate::queries::{NexmarkInput, NexmarkTimer};
 use crate::event::Date;
-use super::maybe_refresh_faster;
-use faster_rs::{FasterKv, status};
+use std::collections::HashMap;
 use tempfile::TempDir;
+use faster_rs::{FasterKv, status};
 
 pub fn q8<S: Scope<Timestamp = usize>>(
     input: &NexmarkInput,
@@ -14,98 +14,104 @@ pub fn q8<S: Scope<Timestamp = usize>>(
     scope: &mut S,
     window_size_ns: usize,
 ) -> Stream<S, usize> {
-    let auctions = input.auctions(scope).map(|a| (a.seller, a.date_time));
+    let auctions = input.auctions(scope).map(move |a| (a.seller, nt.from_nexmark_time(a.date_time)));
 
-    let people = input.auctions(scope).map(|p| (p.id, p.date_time));
+    let people = input.people(scope).map(|p| (p.id, p.date_time));
 
-    people.binary_frontier(
+    let new_people_directory = TempDir::new_in(".").unwrap().into_path();
+    let new_people = FasterKv::new_u64_store(
+        1 << 24,
+        2 * 1024 * 1024 * 1024,
+        new_people_directory.to_str().unwrap().to_string(),
+    )
+        .unwrap();
+    let mut new_people_store_serial = 0;
+    let auctions_state_directory = TempDir::new_in(".").unwrap().into_path();
+    let auctions_state = FasterKv::new_u64_pairs_store(
+        1 << 24,
+        2 * 1024 * 1024 * 1024,
+        auctions_state_directory.to_str().unwrap().to_string(),
+    )
+        .unwrap();
+    let mut auctions_state_store_serial = 0;
+
+    let mut index_state: Vec<usize> = Vec::new();
+    people.binary_notify(
         &auctions,
         Exchange::new(|p: &(usize, _)| p.0 as u64),
         Exchange::new(|a: &(usize, _)| a.0 as u64),
         "Q8 join",
-        |_capability, _info| {
-            let new_people_dir = TempDir::new_in(".").expect("Unable to create FASTER directory");
-            let new_people = FasterKv::new_u64_store(1 << 24, 3 * 1024 * 1024 * 1024, new_people_dir.into_path().to_str().unwrap().to_owned()).unwrap();
-            new_people.start_session();
-            let mut new_people_serial = 0;
-            let mut auctions: Vec<(usize, Vec<(usize, Date)>)> = Vec::new();
+        None,
+        move |input1, input2, output, notificator| {
+            //let mut new_people: HashMap<usize, usize> = HashMap::new();
+            //let mut auctions_state: HashMap<usize, Vec<(usize, Date)>> = HashMap::new();
 
-            let mut delayed = None;
+            // Notice new people.
+            input1.for_each(|time, data| {
+                notificator.notify_at(time.retain());
+                for (person, p_time) in data.iter().cloned() {
+                    new_people.upsert_u64(person as u64, *p_time as u64, new_people_store_serial);
+                    super::maybe_refresh_faster(&new_people, &mut new_people_store_serial);
+                }
+            });
 
-            move |input1, input2, output| {
-                // Notice new people.
-                input1.for_each(|time, data| {
-                    if delayed.is_none() {
-                        delayed = Some(time.delayed(&time.time()));
+            // Notice new auctions.
+            input2.for_each(|time, data| {
+                let ts = *time.time();
+                let mut data_vec = vec![];
+                data.swap(&mut data_vec);
+                /*
+                let mut stored_auctions = auctions_state.remove(&ts).unwrap_or(Vec::new());
+                stored_auctions.extend(data_vec);
+                auctions_state.insert(ts, stored_auctions);
+                */
+                auctions_state.rmw_u64_pairs(ts as u64, data_vec, auctions_state_store_serial);
+                super::maybe_refresh_faster(&auctions_state, &mut auctions_state_store_serial);
+                //auctions_state.entry(ts).or_insert(Vec::new()).append(&mut data_vec);
+                notificator.notify_at(time.retain());
+            });
+
+            notificator.for_each(|cap, _, _| {
+                let capability_time = *cap.time();
+                let mut entries_to_check = index_state.clone();
+                entries_to_check.push(capability_time);
+                let mut to_keep = Vec::new();
+                for ts in entries_to_check { // ts <= capability_time
+                    let (res, recv) = auctions_state.read_u64_pairs(ts as u64, auctions_state_store_serial);
+                    if res == status::PENDING {
+                        auctions_state.complete_pending(true);
                     }
-                    for (person, time) in data.iter().cloned() {
-                        new_people.upsert_u64(person as u64, nt.from_nexmark_time(time) as u64, new_people_serial);
-                        maybe_refresh_faster(&new_people, &mut new_people_serial);
-                    }
-                });
-
-                // Notice new auctions.
-                input2.for_each(|time, data| {
-                    if delayed.is_none() {
-                        delayed = Some(time.delayed(&time.time()));
-                    }
-                    let mut data_vec = vec![];
-                    data.swap(&mut data_vec);
-                    auctions.push((*time.time(), data_vec));
-                });
-
-                // Determine least timestamp we might still see.
-                let complete1 = input1
-                    .frontier
-                    .frontier()
-                    .get(0)
-                    .cloned()
-                    .unwrap_or(usize::max_value());
-                let complete2 = input2
-                    .frontier
-                    .frontier()
-                    .get(0)
-                    .cloned()
-                    .unwrap_or(usize::max_value());
-                let complete = std::cmp::min(complete1, complete2);
-
-                for (auction_time, auctions) in auctions.iter_mut() {
-                    if *auction_time < complete {
-                        {
-                            //let mut session = output.session(&capability);
-                            let mut delayed = delayed.as_ref().unwrap().delayed(auction_time);
-                            let mut session = output.session(&delayed);
-                            for &(person, time) in auctions.iter() {
-                                if time < nt.to_nexmark_time(complete) {
-                                    let (res, recv) = new_people.read_u64(person as u64, new_people_serial);
-                                    if res == status::PENDING {
-                                        new_people.complete_pending(true);
-                                    }
-                                    maybe_refresh_faster(&new_people, &mut new_people_serial);
-                                    if let Ok(p_time) = recv.recv() {
-                                        if *time < (p_time as usize) + window_size_ns {
-                                            session.give(person);
-                                        }
+                    super::maybe_refresh_faster(&auctions_state, &mut auctions_state_store_serial);
+                    if let Some(mut auctions) = recv.recv().ok() {
+                        auctions_state.delete_u64_pairs(ts as u64, auctions_state_store_serial);
+                        super::maybe_refresh_faster(&auctions_state, &mut auctions_state_store_serial);
+                        let mut session = output.session(&cap);
+                        for &(person, time) in auctions.iter() {
+                            if time <= *nt.to_nexmark_time(capability_time) {
+                                let (res, recv) = new_people.read_u64(person as u64, new_people_store_serial);
+                                if res == status::PENDING {
+                                    new_people.complete_pending(true);
+                                }
+                                super::maybe_refresh_faster(&new_people, &mut new_people_store_serial);
+                                if let Some(p_time) = recv.recv().ok() {
+                                    if time < *nt.to_nexmark_time(p_time as usize + window_size_ns) {
+                                        session.give(person);
                                     }
                                 }
                             }
-                            auctions.retain(|&(_, time)| time >= nt.to_nexmark_time(complete));
                         }
-                        if let Some(minimum) = auctions.iter().map(|x| x.1).min() {
-                            //capability.downgrade(&nt.from_nexmark_time(minimum));
+                        auctions.retain(|&(_, time)| time > *nt.to_nexmark_time(capability_time));
+                        if auctions.len() > 0 {
+                            // Put it back in state
+                            auctions_state.upsert_u64_pairs(ts as u64, auctions, auctions_state_store_serial);
+                            super::maybe_refresh_faster(&auctions_state, &mut auctions_state_store_serial);
+                            to_keep.push(ts)
                         }
                     }
                 }
-                delayed.as_mut().map(|cap| cap.downgrade(&complete));
-                if complete == usize::max_value() {
-                    delayed = None;
-                }
-                auctions.retain(|&(_, ref list)| !list.is_empty());
-                // println!("auctions.len: {:?}", auctions.len());
-                // for thing in auctions.iter() {
-                //     println!("\t{:?} (len: {:?}) vs {:?}", thing.0, thing.1.len(), complete);
-                // }
-            }
+                // Update entries
+                index_state = to_keep;
+            });
         },
     )
 }
